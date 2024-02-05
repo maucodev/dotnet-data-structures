7ca5a249c3d3a143224627d29d355110aa0cac8a
maucodev <hola@maucodev.com>
2024-02-04 20:41:28 -0600

    Working with arrays

---
+++ DataStructures/01_Arrays/CustomArray.cs
@@ -0,0 +1,65 @@
+﻿namespace DataStructures._01_Arrays
+{
+    public class CustomArray(int size)
+    {
+        private int[] _array = new int[size];
+
+        private int _length;
+
+        public void Insert(int number)
+        {
+            if (_array.Length <= _length)
+            {
+                IncreaseSize();
+            }
+
+            _array[_length++] = number;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index >= _length)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            for (var i = index; i < _length - 1; i++)
+            {
+                _array[i] = _array[i + 1];
+            }
+
+            _length--;
+        }
+
+        public int IndexOf(int value)
+        {
+            for (var i = 0; i < _length; i++)
+            {
+                if (_array[i] == value)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        private void IncreaseSize()
+        {
+            var relay = new int[_length * 2];
+
+            for (var i = 0; i < _length; i++)
+            {
+                relay[i] = _array[i];
+            }
+
+            _array = relay;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine(_length == 0 ? "[ ]" : string.Join(" - ", _array.Take(_length)));
+            Console.WriteLine();
+        }
+    }
+}
