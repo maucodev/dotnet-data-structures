74760d7fda39717206e476df9f5960d6aa8084fb
maucodev <hola@maucodev.com>
2024-02-06 20:16:05 -0600

    Working with Stacks

--- DataStructures/03_Stacks/CustomStack.cs
+++ DataStructures/03_Stacks/CustomStack.cs
@@ -1,6 +1,64 @@
-﻿namespace DataStructures._03_Stacks;
+﻿using System.Text;
+
+namespace DataStructures._03_Stacks;
 
 public class CustomStack
 {
-    
+    private int _length;
+
+    private readonly int[] _stack = new int[5];
+
+    public bool IsEmpty()
+    {
+        return _length == 0;
+    }
+
+    public bool IsFull()
+    {
+        return _length == _stack.Length;
+    }
+
+    public int Peek()
+    {
+        if (IsEmpty())
+        {
+            throw new StackOverflowException();
+        }
+
+        return _stack[_length - 1];
+    }
+
+    public int Pop()
+    {
+        if (IsEmpty())
+        {
+            throw new StackOverflowException();
+        }
+
+        return _stack[--_length];
+    }
+
+    public void Push(int item)
+    {
+        if (IsFull())
+        {
+            throw new StackOverflowException();
+        }
+
+        _stack[_length++] = item;
+    }
+
+    public override string ToString()
+    {
+        var result = new StringBuilder("[");
+
+        for (var i = 0; i < _length; i++)
+        {
+            result.Append($" {_stack[i]} ");
+        }
+
+        result.Append(']');
+
+        return result.ToString();
+    }
 }
--- DataStructures/03_Stacks/CustomStackTests.cs
+++ DataStructures/03_Stacks/CustomStackTests.cs
@@ -4,16 +4,22 @@ public static class CustomStackTests
 {
     public static void Execute()
     {
-        Console.WriteLine("Testing the Reverse method...\n");
-        Console.WriteLine($"Reverse string result: {StringReverser.Reverse("abc")}\n");
+        var stack = new CustomStack();
 
-        Console.WriteLine("Testing the IsBalanced method...\n");
-        var expression = "(1+2)";
-        Console.WriteLine($"Expression: {expression}");
-        Console.WriteLine($"Is balanced result: {Expression.IsBalanced(expression)}");
-        expression = "(1+2]";
-        Console.WriteLine($"Expression: {expression}");
-        Console.WriteLine($"Is balanced result: {Expression.IsBalanced(expression)}\n");
+        Console.WriteLine("Testing the Push method...\n");
+        stack.Push(10);
+        stack.Push(20);
+        stack.Push(30);
+        Console.WriteLine($"{stack}\n");
+
+        Console.WriteLine("Testing the Pop method...\n");
+        stack.Pop();
+        Console.WriteLine($"{stack}\n");
+
+        Console.WriteLine("Testing the Peek method...\n");
+        Console.WriteLine($"{stack}");
+        Console.WriteLine($"Peeked value: {stack.Peek()}");
+        Console.WriteLine($"{stack}\n");
 
         Console.WriteLine("Finished program");
     }
