db37c0b78970e457dae801ea08f4408ef0e1cf50
maucodev <hola@maucodev.com>
2024-02-11 12:18:11 -0600

    Working with Hash Tables

--- DataStructures/05_HashTables/HashTableTests.cs
+++ DataStructures/05_HashTables/HashTableTests.cs
@@ -15,6 +15,9 @@ namespace DataStructures._05_HashTables
             TestTheFindFirstNonRepeatingCharAlgorithm("a green apple");
             TestTheFirstRepeatedCharAlgorithm("green apple");
             TestHashTable();
+            TestMostFrequent();
+            TestCountPairsWithKDifference();
+            TestTwoSum();
         }
 
         private static void TestTheFindFirstNonRepeatingCharAlgorithm(string input)
@@ -82,6 +85,79 @@ namespace DataStructures._05_HashTables
 
             hashTable.Remove(3);
             Console.WriteLine($"Key = {6}, Value = {hashTable.Get(3) ?? "?"}");
+        }
+
+        private static void TestMostFrequent()
+        {
+            ConsoleUtilities.PrintTitle("Most Frequent");
+
+            var input = new[] { 1, 2, 2, 3, 3, 3, 4 };
+            
+            var hashTable = new Dictionary<int, int>();
+
+            foreach (var item in input)
+            {
+                if (!hashTable.TryAdd(item, 1))
+                {
+                    hashTable[item]++;
+                }
+            }
+
+            Console.WriteLine($"The most frequent value is: {hashTable.MaxBy(item => item.Value).Value}");
+        }
+
+        private static void TestCountPairsWithKDifference(int k = 2)
+        {
+            ConsoleUtilities.PrintTitle("Count Pairs With 'K' Difference");
+
+            var totalPairs = 0;
+            var input = new[] { 1, 7, 5, 9, 2, 12, 3 };
+
+            var hashTable = new HashSet<int>(input);
+
+            foreach (var item in input)
+            {
+                if (hashTable.Contains(item + k))
+                {
+                    totalPairs++;
+                }
+
+                if (hashTable.Contains(item - k))
+                {
+                    totalPairs++;
+                }
+
+                hashTable.Remove(item);
+            }
+
+            Console.WriteLine($"The total pairs with difference of {k} is: {totalPairs}");
+        }
+
+        private static void TestTwoSum(int target = 9)
+        {
+            ConsoleUtilities.PrintTitle("Two SUM");
+
+            var input = new[] { 2, 7, 11, 15 };
+
+            var hashTable = new Dictionary<int, int>();
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                var complement = target - input[i];
+
+                if (hashTable.TryGetValue(complement, out var value))
+                {
+                    var result = new [] { value, i };
+
+                    Console.WriteLine($"The item at index'[{result[0]}]={input[result[0]]} + [{result[1]}]={input[result[1]]} is equal to {target}");
+
+                    return;
+                }
+
+                hashTable.Add(input[i], i);
+            }
+
+            Console.WriteLine($"There are not two elements witch their sum is equal to {target}");
         }
     }
 }
