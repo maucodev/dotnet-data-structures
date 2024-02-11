22351bc909569c973205ad611a33f0126f90bdd7
maucodev <hola@maucodev.com>
2024-02-10 18:19:45 -0600

    Working with Hash Tables

--- DataStructures/05_HashTables/HashTableTests.cs
+++ DataStructures/05_HashTables/HashTableTests.cs
@@ -7,6 +7,7 @@ public static class HashTableTests
     public static void Execute()
     {
         FindFirstNonRepeatingChar("a green apple");
+        FirstRepeatedChar("green apple");
     }
 
     private static void FindFirstNonRepeatingChar(string input)
@@ -28,7 +29,27 @@ public static class HashTableTests
         var result = hashTable.MinBy(item => item.Value);
 
         Console.WriteLine($"Input: {input}\n");
-
+        
         Console.WriteLine($"The first non repeating char is '{result.Key}' with {result.Value} occurrences");
+    }
+
+    private static void FirstRepeatedChar(string input)
+    {
+        ConsoleUtilities.PrintTitle("First Repeated Char");
+
+        if (string.IsNullOrEmpty(input))
+        {
+            throw new InvalidOperationException("The input is invalid");
+        }
+
+        var hashTable = new HashSet<char>();
+
+        var repeatedChar = input.FirstOrDefault(item => !hashTable.Add(item));
+
+        Console.WriteLine($"Input: {input}\n");
+
+        Console.WriteLine(repeatedChar == default
+            ? "There are not repeated char"
+            : $"The first repeated char is '{repeatedChar}'");
     }
 }
